570b844b1a897ef525acd3373f3be35711e63584
Petrov Andrey <4ndrew1337@mail.ru>
2026-01-22 23:00:58 +0300

    add migrations

---
+++ migrations/20260111160916_notifications.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS notifications
+(
+    uid         UUID        PRIMARY KEY,
+    payload     JSONB       NOT NULL,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    send_at     TIMESTAMPTZ NOT NULL,
+    status      TEXT        NOT NULL,
+    retry_count INT         NOT NULL DEFAULT 0
+);
+
+CREATE INDEX idx_pending_notifications
+ON notifications (send_at ASC)
+WHERE status = 'pending';
