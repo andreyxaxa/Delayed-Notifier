efe23df4a4a80f88fe87797da3d4710af8588230
Petrov Andrey <4ndrew1337@mail.ru>
2026-01-22 17:58:20 +0300

    add config: http, pg, redis, log, rmq

---
+++ config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type (
+	Config struct {
+		HTTP  HTTP
+		PG    PG
+		Redis Redis
+		Log   Log
+		RMQ   RMQ
+	}
+
+	HTTP struct {
+		Port string `env:"HTTP_PORT,required"`
+	}
+
+	PG struct {
+		PoolMax int    `env:"PG_POOL_MAX,required"`
+		URL     string `env:"PG_URL,required"`
+	}
+
+	Redis struct {
+		Addr        string `env:"REDIS_ADDR,required"`
+		DB          int    `env:"REDIS_DB,required"`
+		User        string `env:"REDIS_USER"`
+		Password    string `env:"REDIS_PASSWORD"`
+		DialTimeout int    `env:"REDIS_DIAL_TIMEOUT"`
+		Timeout     int    `env:"REDIS_TIMEOUT"`
+	}
+
+	Log struct {
+		Level string `env:"LOG_LEVEL,required"`
+	}
+
+	RMQ struct {
+		URL           string `env:"RMQ_URL,required"`
+		Queue         string `env:"RMQ_QUEUE"`
+		Exchange      string `env:"RMQ_EXCHANGE"`
+		RoutingKey    string `env:"RMQ_ROUTING_KEY"`
+		Workers       int    `env:"RMQ_WORKERS"`
+		PrefetchCount int    `env:"RMQ_PREFETCH_COUNT"`
+	}
+)
+
+func New() (*Config, error) {
+	cfg := &Config{}
+
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("config error: %v", err)
+	}
+
+	return cfg, nil
+}
