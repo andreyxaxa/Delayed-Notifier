9fc847192fdc235c537651dd2bf70ee80aff10ee
Petrov Andrey <4ndrew1337@mail.ru>
2026-01-22 19:59:37 +0300

    updated app: created and injected rabbitmq publisher; created and started background worker

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,15 +6,18 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/andreyxaxa/Delayed-Notifier/config"
 	"github.com/andreyxaxa/Delayed-Notifier/internal/controller/rabbitmq"
 	"github.com/andreyxaxa/Delayed-Notifier/internal/controller/restapi"
 	mailsender "github.com/andreyxaxa/Delayed-Notifier/internal/infrastructure/mail_sender"
+	rmqpublisher "github.com/andreyxaxa/Delayed-Notifier/internal/infrastructure/rmq_publisher"
 	telegramsender "github.com/andreyxaxa/Delayed-Notifier/internal/infrastructure/telegram_sender"
 	"github.com/andreyxaxa/Delayed-Notifier/internal/repo/cache"
 	"github.com/andreyxaxa/Delayed-Notifier/internal/repo/persistent"
 	"github.com/andreyxaxa/Delayed-Notifier/internal/usecase/notification"
+	workerpublisher "github.com/andreyxaxa/Delayed-Notifier/internal/worker/worker_publisher"
 	"github.com/andreyxaxa/Delayed-Notifier/pkg/httpserver"
 	"github.com/andreyxaxa/Delayed-Notifier/pkg/logger"
 	"github.com/andreyxaxa/Delayed-Notifier/pkg/postgres"
@@ -82,6 +85,21 @@ func Run(cfg *config.Config) {
 	rmqServer.Start(ctx)
 	httpServer.Start()
 
+	// RabbitMQ Publisher
+	publisher, err := rmqpublisher.New(rmqServer.Client)
+	if err != nil {
+		l.Fatal(fmt.Errorf("app - Run - publisher.New: %v", err))
+	}
+
+	// RabbitMQ worker-publisher
+	rmqPubWorker := workerpublisher.New(publisher, notificationUseCase, l)
+
+	// start worker
+	err = rmqPubWorker.Start(ctx)
+	if err != nil {
+		l.Fatal(fmt.Errorf("app - Run - rmqPubWorker.Start: %v", err))
+	}
+
 	// Waiting signal
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
@@ -91,10 +109,25 @@ func Run(cfg *config.Config) {
 		l.Info("app - Run - signal: %s", s.String())
 	case err := <-httpServer.Notify():
 		l.Error(fmt.Errorf("app - Run - httpServer.Notify: %v", err))
+	case err = <-rmqServer.Notify():
+		l.Error(fmt.Errorf("app - Run - rmqServer.Notify: %v", err))
 	}
 
 	err = httpServer.Shutdown()
 	if err != nil {
 		l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %v", err))
+	}
+
+	err = rmqServer.Shutdown()
+	if err != nil {
+		l.Error(fmt.Errorf("app - Run - rmqServer.Shutdown: %v", err))
+	}
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), time.Second*time.Duration(cfg.Worker.ShutdownTimeout))
+	defer shutdownCancel()
+
+	err = rmqPubWorker.Shutdown(shutdownCtx)
+	if err != nil {
+		l.Error(fmt.Errorf("app - Run - rmqPubWorker.Shutdown: %v", err))
 	}
 }
